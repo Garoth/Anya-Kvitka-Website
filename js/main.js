b8618834bbd0758905b175aba543ae55047d4834
Andrei Thorp <athorp@evernote.com>
2013-10-06 14:19:45 -0700

    Whatever nav stuff I was up to
    Signed-off-by: Andrei Thorp <athorp@evernote.com>

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
-var Anya = {};
+var OrderingError = function() {
+    console.log("DEV SCREWED UP LOADING ORDER! BEAT HIM IMMEDIATELY!");
+};
+
+var Anya = {
+    Debug: OrderingError,
+    Util: OrderingError,
+    Parts: OrderingError,
+    ScrollNav: OrderingError,
+    Load: OrderingError,
+    Main: OrderingError
+};
 
 jQuery.fn.center = function () {
     this.css("position","absolute");
@@ -27,6 +38,7 @@ Anya.Parts = function() {
     me.FadeSpeed = 500;
 
     $(function() {
+        /* List of javascript-interacting objects */
         me.Logo = $(".logo");
         me.Main = $("#main");
         me.Pages = {
@@ -36,7 +48,81 @@ Anya.Parts = function() {
         me.PageLinks = {
             about: $("#about-link")
         };
+        me.ScrollNav = $("#scrollnav");
+
+        /* Particular page abilities */
+        me.Pages.slideshow.Load = function() {
+            img1 = $(".image1", me.Pages.slideshow);
+            img2 = $(".image2", me.Pages.slideshow);
+            img3 = $(".image3", me.Pages.slideshow);
+
+            Anya.ScrollNav.Add(img1);
+            Anya.ScrollNav.Add(img2);
+            Anya.ScrollNav.Add(img3);
+            Anya.ScrollNav.Start();
+
+            Anya.Debug("Loaded slideshow page");
+        };
+
+        me.Pages.slideshow.Unload = function() {
+            Anya.ScrollNav.Stop();
+            Anya.Debug("Unloaded slideshow page");
+        }
+
+        me.Pages.about.Load = function() {
+            Anya.Debug("Loaded about page");
+        };
+
+        me.Pages.about.Unload = function() {
+            Anya.Debug("Unloaded about page");
+        };
     });
+
+    return me;
+}();
+
+Anya.ScrollNav = function() {
+    var me = {};
+
+    var elements = [];
+    var intervalId = null;
+    var currentElement = 0;
+
+    me.Add = function(element, callback) {
+        elements.push([element, callback]);
+    };
+
+    me.Clear = function() {
+        elements = [];
+        running = false;
+    };
+
+    me.Start = function() {
+        Anya.Debug("Starting Auto ScrollNav");
+
+        intervalId = setTimeout(function() {
+            if (elements.length < 2) {
+                return;
+            }
+
+            var oldElem = elements[currentElement][0];
+            currentElement = (currentElement + 1) % elements.length;
+            var newelem = elements[currentElement][0];
+
+            Anya.Load.GenericTransition(oldElem, newElem);
+
+            if (elements[currentElement][1] != null) {
+                elements[currentElement][1]();
+            }
+        }, 2000);
+    };
+
+    me.Stop = function() {
+        Anya.Debug("Stopping Auto ScrollNav");
+
+        clearInterval(intervalId);
+        intervalId = null;
+    };
 
     return me;
 }();
@@ -49,16 +135,24 @@ Anya.Load = function() {
         Anya.Parts.Logo.center();
         Anya.Parts.Main.center();
 
-        Anya.Parts.Logo.fadeIn(Anya.Parts.FadeSpeed, function() {
+        Anya.Parts.Logo.fadeIn(Anya.Parts.FadeSpeed * 3, function() {
             Anya.Parts.Main.fadeIn(Anya.Parts.FadeSpeed, function() {
                 Anya.Parts.Logo.hide();
             });
         });
     };
 
+    me.GenericTransition = function(oldElement, newElement) {
+        oldElement.fadeOut(Anya.Parts.FadeSpeed);
+        newElement.fadeIn(Anya.Parts.FadeSpeed);
+    };
+
     me.Page = function(pageid) {
-        Anya.Parts.Pages[currentPage].fadeOut(Anya.Parts.FadeSpeed);
-        Anya.Parts.Pages[pageid].fadeIn(Anya.Parts.FadeSpeed);
+        var curPage = Anya.Parts.Pages[currentPage];
+        var newPage = Anya.Parts.Pages[pageid];
+        me.GenericTransition(curPage, newPage);
+        curPage.Unload();
+        newPage.Load();
         currentPage = pageid;
     };
 
@@ -72,7 +166,6 @@ Anya.Main = function() {
         Anya.Load.StartSite();
 
         Anya.Parts.PageLinks.about.click(function() {
-            console.log("Clicked on about");
             Anya.Load.Page("about");
         });
     });
