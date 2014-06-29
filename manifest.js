9fc0093325f77df243861eb9151dd4504da61e7a
Andrei Thorp <athorp@evernote.com>
2014-06-28 19:43:15 -0700

    WIP manifest thing
    Signed-off-by: Andrei Thorp <athorp@evernote.com>

---
+++ manifest.js
@@ -0,0 +1,15 @@
+var MANIFEST = {};
+
+MANIFEST.newsTitle = "New & Upcoming";
+
+MANIFEST.newsSubtitle = "Upcoming Shows";
+
+MANIFEST.news = [
+    {
+        title: "Opening for Mike Eagle",
+        link: "http://www.facebook.com/l/rAQHX_3Y4AQFtWp4oF2woH6dOgQIHXle0v4LGYZd22nYhWQ/vivalahiphop.com/2014/06/23/the-mike-eagle-show-at-elbo-room/",
+        date: "July 1st",
+        location: "Elbo Room<br/>@ San Francisco, CA",
+        restrictions: "21+"
+    }
+];
